be5372e9de45bba903a765b4710511f711b47455
sharifjonakramov9.bit <sharifjonakramov9@gemail.com>
2025-11-03 11:14:13 +0300

    idk

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import json
+
 import requests
+
 from config import TOKEN
 
 TG_BOT_URL = f'https://api.telegram.org/bot{TOKEN}'
@@ -64,29 +66,56 @@ def main():
         for update in get_updates(offset, limit):
             if 'message' in update:
                 chat_id = update['message']['chat']['id']
+                message = update['message']
 
-                if 'text' in update['message']:
-                    text = update['message']['text']
-                    user = update['message']['from']
-                    if text == '/start':
-                        new_user = {
-                            'tg_id': user['id'],
-                            'first_name': user['first_name'],
-                            'last_name': user.get('last_name'),
-                            'username': user.get('username')
-                        }
-                        new_user = save_user(new_user)
-                        text = 'salom, botga xush kelibsiz!'
-                        if new_user is None:
-                            text = 'salom, qaytganingiz bilan!'
+                if 'text' in message and message['text'] == '/start':
+                    user = message['from']
+                    new_user = {
+                        'tg_id': user['id'],
+                        'first_name': user['first_name'],
+                        'last_name': user.get('last_name'),
+                        'username': user.get('username')
+                    }
+                    new_user = save_user(new_user)
 
+                    text = 'Salom, botga xush kelibsiz!' if new_user else 'Salom, qaytganingiz bilan!'
                     send_message(chat_id, text)
-                elif 'photo' in update['message']:
-                    photo = update['message']['photo'][-1]
-                    send_photo(chat_id, photo['file_id'])
-                elif 'location' in update['message']:
-                    location = update['message']['location']
-                    send_location(chat_id, location['latitude'], location['longitude'])
+
+                elif 'text' in message:
+                    send_message(chat_id, message['text'])
+
+                elif 'photo' in message:
+                    photo = message['photo'][-1]['file_id']
+                    send_photo(chat_id, photo)
+
+                elif 'sticker' in message:
+                    sticker = message['sticker']['file_id']
+                    requests.get(f"{TG_BOT_URL}/sendSticker?chat_id={chat_id}/sticker={sticker}")
+
+                elif 'voice' in message:
+                    voice = message['voice']['file_id']
+                    requests.get(f"{TG_BOT_URL}/sendVoice?chat_id={chat_id}/voice={voice}")
+
+                elif 'audio' in message:
+                    audio = message['audio']['file_id']
+                    requests.get(f"{TG_BOT_URL}/sendAudio?chat_id={chat_id}/audio={audio}")
+
+                elif 'document' in message:
+                    document = message['document']['file_id']
+                    requests.get(f"{TG_BOT_URL}/sendDocument?chat_id={chat_id}/document={document}")
+
+                elif 'video' in message:
+                    video = message['video']['file_id']
+                    requests.get(f"{TG_BOT_URL}/sendVideo?chat_id={chat_id}/video={video}")
+
+                elif 'contact' in message:
+                    contact = message['contact']
+                    requests.get(
+                        f"{TG_BOT_URL}/sendContact?chat_id={chat_id}/phone_number={contact['phone_number']}/first_name={contact['first_name']}"
+                    )
+
+                elif 'dice' in message:
+                    requests.get(f"{TG_BOT_URL}/sendDice?chat_id={chat_id}")
 
             offset = update['update_id'] + 1
 
